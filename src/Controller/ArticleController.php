98bc0f87b77c4e80728190c85420e7328900746b
David Laignelet <laignelet.david@gmail.com>
2023-04-27 06:34:00 +0200

    twig article
    # BW-34
    Fin structure article show et show_all

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -10,11 +10,21 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/article', name: 'app_article_')]
 class ArticleController extends AbstractController
 {
+    #[Route('/', name: 'show_all')]
+    public function showAll(): Response
+    {
+        return $this->render('article/show_all.html.twig', [
+            'title_page' => 'ShowAll',
+            'text_page' => 'TextPage',
+        ]);
+    }
+
     #[Route('/{slug}', name: 'show')]
     public function show(): Response
     {
         return $this->render('article/show.html.twig', [
-            'controller_name' => 'ArticleController',
+            'title_page' => 'ShowArticle',
+            'text_page' => 'TextPage',
         ]);
     }
 }
