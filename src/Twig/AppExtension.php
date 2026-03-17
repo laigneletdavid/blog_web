a2a0d74d9a5f8b64a30799f14ccb8ea1617f9ce6
David Laignelet <laignelet.david@gmail.com>
2026-03-17 17:20:39 +0100

    Feat : Add sommaire auto

--- src/Twig/AppExtension.php
+++ src/Twig/AppExtension.php
@@ -6,6 +6,7 @@ use App\Entity\Menu;
 use Symfony\Component\Routing\RouterInterface;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFilter;
+use Twig\TwigFunction;
 
 class AppExtension extends AbstractExtension
 {
@@ -21,7 +22,82 @@ class AppExtension extends AbstractExtension
             new TwigFilter('menuLink', [$this, 'menuLink']),
             new TwigFilter('readingTime', [$this, 'readingTime']),
             new TwigFilter('highlight', [$this, 'highlight'], ['is_safe' => ['html']]),
+            new TwigFilter('toc_anchors', [$this, 'addTocAnchors'], ['is_safe' => ['html']]),
         ];
+    }
+
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction('toc_extract', [$this, 'extractToc']),
+        ];
+    }
+
+    /**
+     * Parse HTML content, extract H2/H3, add id anchors.
+     * Returns modified HTML with id attributes on headings.
+     */
+    public function addTocAnchors(?string $html): string
+    {
+        if (!$html) {
+            return '';
+        }
+
+        return preg_replace_callback(
+            '/<(h[23])([^>]*)>(.*?)<\/\1>/is',
+            function (array $m): string {
+                $tag = $m[1];
+                $attrs = $m[2];
+                $text = $m[3];
+                $id = $this->slugify(strip_tags($text));
+
+                // Don't double-add id if already present
+                if (preg_match('/\bid\s*=/i', $attrs)) {
+                    return $m[0];
+                }
+
+                return sprintf('<%s%s id="%s">%s</%s>', $tag, $attrs, $id, $text, $tag);
+            },
+            $html
+        );
+    }
+
+    /**
+     * Extract TOC items from HTML content.
+     * Returns array of ['id' => string, 'text' => string, 'level' => 2|3].
+     * Returns empty array if fewer than 3 headings.
+     */
+    public function extractToc(?string $html, int $minHeadings = 3): array
+    {
+        if (!$html) {
+            return [];
+        }
+
+        $items = [];
+        preg_match_all('/<(h[23])[^>]*>(.*?)<\/\1>/is', $html, $matches, PREG_SET_ORDER);
+
+        if (count($matches) < $minHeadings) {
+            return [];
+        }
+
+        foreach ($matches as $match) {
+            $text = strip_tags($match[2]);
+            $items[] = [
+                'id' => $this->slugify($text),
+                'text' => $text,
+                'level' => (int) $match[1][1], // '2' or '3'
+            ];
+        }
+
+        return $items;
+    }
+
+    private function slugify(string $text): string
+    {
+        $text = transliterator_transliterate('Any-Latin; Latin-ASCII; Lower()', $text);
+        $text = preg_replace('/[^a-z0-9]+/', '-', $text);
+
+        return trim($text, '-');
     }
 
     /**
